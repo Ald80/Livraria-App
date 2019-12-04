02b93bffe96ec2db5e05b7d5ecd71a3a98dcf310
Aldrick <aldrick.kronos@hotmail.com>
2019-12-04 20:26:44 -0300

    Library App

---
+++ book/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class Book(models.Model):
+    nome = models.CharField(max_length= 50)
+    imagem = models.ImageField()
+    autor = models.CharField(max_length= 30, default='desconhecido')
+    email = models.EmailField(blank= True)
+    descricao = models.TextField()
+
+    def __str__(self):
+        return self.name
